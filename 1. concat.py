db67674b593a2965715c68ec63e3e60d02f79aa3
Guido <157439519+GuidoHBeneduzzi@users.noreply.github.com>
2025-06-11 19:07:44 -0300

    Delete 1. concat.py

--- 1. concat.py
+++
@@ -1,7 +0,0 @@
-print("Hello, i'm clodsire, the chat bot to contactenate words two words!")
-word1 = input("Word number 1: ")
-word2 = input("Word number 2: ")
-
-print(word1 + " " + word2)
-
-print("You're welcome! hehe.")
